942a11b80bf3acfcc27d6de5ecf9aeb368383120
Colin Cross <ccross@android.com>
2010-04-07 12:08:27 -0700

    fs: partitions: Fix warnings in fs/partitions/check.c
    Signed-off-by: Colin Cross <ccross@android.com>

--- fs/partitions/check.c
+++ fs/partitions/check.c
@@ -317,6 +317,14 @@ static void part_release(struct device *dev)
 	kfree(p);
 }
 
+static int part_uevent(struct device *dev, struct kobj_uevent_env *env)
+{
+	struct hd_struct *part = dev_to_part(dev);
+
+	add_uevent_var(env, "PARTN=%u", part->partno);
+	return 0;
+}
+
 struct device_type part_type = {
 	.name		= "partition",
 	.groups		= part_attr_groups,
